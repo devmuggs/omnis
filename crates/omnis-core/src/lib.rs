ca151f32bfbf0c54048ed28d8e9c3592860a990c
Tristan Muggridge <muggridge.dev@gmail.com>
2026-03-20 18:12:59 +0900

    parsing yaml to intermediary representation

--- crates/omnis-core/src/lib.rs
+++ crates/omnis-core/src/lib.rs
@@ -1,3 +1,21 @@
-fn hola() {
-    println!("hola!");
+pub mod ir;
+pub mod parser;
+
+pub use ir::OmnisCollection;
+pub use parser::parse_raw_yaml;
+
+use tera::{Context, Tera};
+
+pub fn render_emitter(
+    ir: OmnisCollection,
+    template_str: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let mut tera = Tera::default();
+
+    tera.add_raw_template("emitter", template_str)?;
+
+    let context = Context::from_serialize(&ir)?;
+
+    let result = tera.render("emitter", &context)?;
+    Ok(result)
 }
